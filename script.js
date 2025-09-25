d377bc1401b01a102d0f4fcc5361da36e2f8af67
Mamidi Saikrishna <151380948+thisisSaikrishna@users.noreply.github.com>
2025-09-25 10:58:07 +0530

    Upload complete portfolio project files

---
+++ script.js
@@ -0,0 +1,22 @@
+// script.js
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 150;
+        const sectionHeight = section.clientHeight;
+        if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+};
